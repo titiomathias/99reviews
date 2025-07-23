18d8abb3d9052c6abc308135df523a678dddf02f
Matheus de Alencar <matheusdealencar.contato@gmail.com>
2025-07-23 10:42:20 -0300

    feature: dotenv for puzzle

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import os
-import json
-import logging
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from fastapi import FastAPI, HTTPException, Request
 from scripts import request_site
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
 
 app = FastAPI(
     title="99reviews API",
     description="API para coletar e retornar reviews do 99freelas",
-    version="2.0.1",
-    docs_url=None,
-    redoc_url=None,
-    openapi_url=None,
+    version="3.0.0"
 )
+
+limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,6 +28,14 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"99reviews": "Made by: github.com/titiomathias"}
+
+
+@app.post("/flag")
+def read_root(password: str):
+    if password == config["PASSWORD"]:
+        return {"flag": config["FLAG"]}
+    else:
+        return {"status": 400, "message": "wrong password!"}
 
 
 @app.get("/reviews", summary="Get Reviews", tags=["Reviews"])
